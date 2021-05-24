f41c227e9db105c083457dc52b4feba07e7b7689
Wilby Mujuni <23701282+wilby-mj@users.noreply.github.com>
2021-05-24 17:26:49 +0300

    Cosmetics
    Updated the layout and added ID tags for JS functions.

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
 # Forex Risk Management Calculator
 A comprehensive calculation tool to determine position size and project potential profits (or losses) that will help the user manage their capital effectively.
 
-This is not a final calculation, broker fees such as spread and comission are not included.
+Disclaimer: This is not a final calculation, broker fees such as spread, swap and comission are not included.
+
+This web app is built based on Bootstrap (HTML & CSS) and Vue.js (JavaScript).
--- js/script.js
+++ js/script.js
@@ -1,1 +1,20 @@
+calculateMargin(){
+    let margin;
+    let LotSize = document.getElementById("LotSize").innerHTML;
+}
+
+changeDirection(){
+    let direction = document.getElementById("BuySellButton").innerHTML;
+    if (direction){
+        direction == 1; //Value == 1 is SELL, set button Red
+        return
+    } else {
+        //Value == 0 is BUY, set button Green
+    }
+}
+
+calculateProfitLoss(){
+    //when button == BUY; Profit = Exit - Entry, Loss = Entry - Exit
+    //when button == SELL; Profit = Entry - Exit, Loss = Exit - Entry
+}
 
