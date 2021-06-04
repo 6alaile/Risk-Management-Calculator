3d9d3ab1808636813f861467c3e33ec9655a0761
Wilby Mujuni <23701282+wilby-mj@users.noreply.github.com>
2021-06-04 06:34:53 +0300

    Logic
    Changes to HTML references for calculation components.
    Changes to JavaScript logic.

--- js/script.js
+++ js/script.js
@@ -1,30 +1,37 @@
-const BaseCurrency = document.getElementById("AccountCurrency");
-const CurrencyPair = document.getElementById("CurrencyPair");
-const LotSize = document.getElementById("LotSize");
+BaseCurrency = document.getElementById("AccountCurrency");
+CurrencyPair = document.getElementById("CurrencyPair");
+LotSize = document.getElementById("LotSize");
 
-function calculateMargin(){
+function calculateMargin(LotSize, Leverage){
     /* 
     Margin = (LotSize*100000)/Leverage * Exchange Rate
     Exchange Rate = Account currency vs Quote Currency (import via api)
     */
-    Leverage = document.getElementById("SelectedLeverage"); 
+    Leverage = document.getElementById("SelectedLeverage");
     Margin = (LotSize * 100000) / Leverage;
-
-    document.getElementById("MarginResult").innerHTML = Margin;
+    return Margin;
 }
+document.getElementById("MarginResult").innerHTML = calculateMargin(LotSize, Leverage);
 
-function changeDirection(){
-    direction = document.getElementById("BuySellButton")s;
-    if (direction == 1){
-         //set button Red and text SELL
-        return
-    } else (direction == 0) {
-        //set button Green and text BUY
+
+EntryPrice = document.getElementById("EntryPrice");
+TakeProfit = document.getElementById("TakeProfit");
+StopLoss = document.getElementById("StopLoss");
+
+function calculateBuy(BuyRadioButton){
+    
+    if (TakeProfit > EntryPrice > StopLoss) {
+        Profit = TakeProfit - EntryPrice;
+        Loss = EntryPrice - StopLoss;
+    } else {
+        
     }
+    //BuyRadioButton: Profit = Exit > Entry, Loss = Entry > Exit
+    //SellRadioButton: Profit = Entry > Exit, Loss = Exit > Entry
 }
 
-function calculateProfitLoss(){
-    //when button == BUY; Profit = Exit - Entry, Loss = Entry - Exit
-    //when button == SELL; Profit = Entry - Exit, Loss = Exit - Entry
+function calculateSell(SellRadioButton){
+    Profit = EntryPrice - TakeProfit
+    Loss = StopLoss - EntryPrice
 }
 
