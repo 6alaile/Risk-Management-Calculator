0aacca30b69e5df49a173762e1e43143c751b8ff
Wilby Mujuni <23701282+wilby-mj@users.noreply.github.com>
2021-06-04 23:19:37 +0300

    Comments on JS logic

--- js/script.js
+++ js/script.js
@@ -2,11 +2,16 @@ BaseCurrency = document.getElementById("AccountCurrency");
 CurrencyPair = document.getElementById("CurrencyPair");
 LotSize = document.getElementById("LotSize");
 
-function calculateMargin(LotSize, Leverage){
-    /* 
-    Margin = (LotSize*100000)/Leverage * Exchange Rate
+/*
+
+function to calculate margin required to open position(){
     Exchange Rate = Account currency vs Quote Currency (import via api)
-    */
+    Margin = ((LotSize*100000)/Leverage) * Exchange Rate
+}
+*/
+
+function calculateMargin(LotSize, Leverage){
+
     Leverage = document.getElementById("SelectedLeverage");
     Margin = (LotSize * 100000) / Leverage;
     return Margin;
@@ -18,20 +23,37 @@ EntryPrice = document.getElementById("EntryPrice");
 TakeProfit = document.getElementById("TakeProfit");
 StopLoss = document.getElementById("StopLoss");
 
+/* 
+
+function calculateProfitLoss(EntryPrice, TakeProfit, StopLoss){
+    while (button selected = buyradiobutton){
+        Profit = TakeProfit - EntryPrice;
+        Loss = EntryPrice - StopLoss;
+    }
+    while (button selected = sellradiobutton){
+        Profit = EntryPrice - TakeProfit;
+        Loss = StopLoss - EntryPrice;
+    }
+
+    return ;
+
+    write result into CalculatedProfit & CalcuatedLoss (disabled input fields)
+}
+
+*/
+
 function calculateBuy(BuyRadioButton){
     
     if (TakeProfit > EntryPrice > StopLoss) {
-        Profit = TakeProfit - EntryPrice;
-        Loss = EntryPrice - StopLoss;
+        
     } else {
         
     }
-    //BuyRadioButton: Profit = Exit > Entry, Loss = Entry > Exit
-    //SellRadioButton: Profit = Entry > Exit, Loss = Exit > Entry
+    //BuyRadioButton - Profit: Exit > Entry, Loss: Entry > Exit
+    //SellRadioButton- Profit: Entry > Exit, Loss: Exit > Entry
 }
 
 function calculateSell(SellRadioButton){
-    Profit = EntryPrice - TakeProfit
-    Loss = StopLoss - EntryPrice
+    
 }
 
