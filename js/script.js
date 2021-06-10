674bb1b1a0b4e6b41352d7314cdeac77104a295d
Wilby Mujuni <23701282+wilby-mj@users.noreply.github.com>
2021-06-10 19:59:28 +0300

    Edits to logic

--- js/script.js
+++ js/script.js
@@ -1,18 +1,15 @@
 const AccountCurrency = document.getElementById("AccountCurrency");
-
 function setAccountCurrency();
 
-
 const CurrencyPair = document.getElementById("CurrencyPair");
-
 function selectedCurrency();
-
 
 const LotSize = document.getElementById("LotSize");
 
-
 /*
-function to calculate margin required to open position(){
+calculate margin required to open position
+
+function calculateMargin(){
     Exchange Rate = Account currency vs Quote Currency (import via api)
     Margin = ((LotSize*100000)/Leverage) * Exchange Rate
 }
@@ -26,31 +23,36 @@ function calculateMargin(LotSize, Leverage){
 document.getElementById("MarginResult").innerHTML = calculateMargin(LotSize, Leverage);
 
 
-var EntryPrice = document.getElementById("EntryPrice");
 
-var BuySellButton = document.getElementsByName("BuySellButton"){
-    Buy = document.getElementById("BuyRadioButton"),
-    Sell = document.getElementById("SellRadioButton")
-}
+var EntryPrice = document.getElementById("EntryPrice");
+var BuySellButton = {                                       //document.getElementsByName("BuySellButton")
+        Buy = document.getElementById("BuyRadioButton"),
+        Sell = document.getElementById("SellRadioButton")
+    }
+
+var TakeProfit = document.getElementById("TakeProfit");     // var TPSL = document.getElementsByName("LimitValue");
+var StopLoss = document.getElementById("StopLoss");         // var TPSL = document.getElementsByName("LimitValue");
+
+var Profit = document.getElementById("CalculatedProfit");   // var calculatedProfitLoss = document.getElementsByName("result");
+var Loss = document.getElementById("CalculatedLoss");       // var calculatedProfitLoss = document.getElementsByName("result");
 
 function ProfitLoss(BuySellButton){
     if(BuySellButton = Buy){
-        //BuyRadioButton - Profit: Exit > Entry, Loss: Entry > Exit
+        //BuyRadioButton (TakeProfit > EntryPrice > StopLoss): Profit = TakeProfit - Entry, Loss = Entry - TakeProfit;
+        Profit = TakeProfit - EntryPrice;
+        Loss = EntryPrice - TakeProfit; 
+        return;
+    } 
+    else if (BuySellButton = Sell) {
+        //SellRadioButton (StopLoss > EntryPrice > TakeProfit): Profit = Entry - TakeProfit, Loss = TakeProfit - Entry;
 
         return;
-    }else if (BuySellButton = Sell) {
-        //SellRadioButton- Profit: Entry > Exit, Loss: Exit > Entry
+    } 
+    else {
 
-        return;
-    } else {
         return;
     }
 };
-
-
-var TPSL = document.getElementsByName("LimitValue");
-var TakeProfit = document.getElementById("TakeProfit");
-var StopLoss = document.getElementById("StopLoss");
 
 
 /* 
@@ -69,7 +71,3 @@ function calculateProfitLoss(EntryPrice, TakeProfit, StopLoss){
     write result into CalculatedProfit & CalcuatedLoss (disabled input fields)
 }
 */
-var calculatedProfitLoss = document.getElementsByName("result");
-var Profit = document.getElementById("CalculatedProfit");
-var Loss = document.getElementById("CalculatedLoss");
-
