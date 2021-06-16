9c3ee77b192cc73f3f84f877fc34b8b2883c9cf7
wilby-mj <23701282+wilby-mj@users.noreply.github.com>
2021-06-16 03:04:54 +0300

    Added money.js and accounting.js minified libs

--- js/script.js
+++ js/script.js
@@ -1,6 +1,5 @@
 var AccountCurrency = document.getElementById("AccountCurrency");
 var CurrencyPair = document.getElementById("CurrencyPair");
-var AccountBalance = document.getElementById("AccountBalance").value;
 
 // Function to calculate Margin Required to Open a Position
 
@@ -18,7 +17,6 @@ the exchange rate of the Quote Currency to the AccountCurrency.
 AccountMargin = QuoteMargin * Exchange Rate[Quote Currency/Account Currency]
 */
 
-
 function calculateMargin(){
     var LotSize = document.getElementById("LotSize").value;
     var Leverage = document.getElementById("SelectedLeverage").value;
@@ -29,9 +27,11 @@ function calculateMargin(){
     // console.log(AccountMargin);
 
     document.getElementById("AccountMargin").value = QuoteMargin;
+
+    // return AccountMargin;
 }
 
-//
+// Function to determine if account balance exceedes margin
 
 /*
 Finally to determine if the defined account balance can open the position based on the
@@ -40,20 +40,24 @@ textbox should turn red if larger than the account Balance, and green if less th
 
 AccountBalance >= AccountMargin {Green}, AccountBalance <= AccountMargin {Red}
 */
-/*
+
 function compareAccount(){
+    var AccountBalance = document.getElementById("AccountBalance").value;
+
     if (AccountMargin >= AccountBalance){
-        document.getElementById("AccountMargin").style;
+        document.getElementById("AccountMargin").style = red;
     }else if(AccountMargin <= AccountBalance){
-        return Green;
+        document.getElementById("AccountMargin").style = Green;
     }
 };
-*/
+
 // Function to calculate the value of one pip
 
-/**/
+/*
+
+*/
 function valuePerPip(){
-    var OnePip = LotSize/ExchangeRate ;
+    var OnePip = (LotSize/ExchangeRate) * 100000 ;
     
     document.getElementById("PipValue").innerHTML = OnePip;
 };
@@ -70,7 +74,7 @@ Profit = TakeProfit - EntryPrice, Loss = StopLoss - EntryPrice
 If SELL is selected:
 Profit = EntryPrice - TakeProfit, Loss = EntryPrice - StopLoss
 
-Then onvert the profit/loss into the account currency:
+Then convert the profit/loss into the account currency:
 AccountProfit = Profit * QuoteCurrency/AccountCurrency
 
 Finally print the AccountProfit or AccountLoss into the form.
@@ -113,5 +117,6 @@ function ProfitLoss(BuySellButton){
     } 
     else{
         return null;
+        // Figure out how to round Profit and Loss to 2 decimals
     }
 };
